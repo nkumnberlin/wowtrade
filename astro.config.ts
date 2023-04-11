d147c2049a6a09ddda03baac9857108dc5d25478
nkumnick <nkumnick@oev.de>
2023-04-11 10:11:51 +0200

    modifiy vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,43 +1,50 @@
-import { defineConfig } from "astro/config"; // Astro integration imports
+import { defineConfig } from "astro/config" // Astro integration imports
 
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
-import { VitePWA } from "vite-plugin-pwa"; // Helper imports
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import compress from "astro-compress"
+import { VitePWA } from "vite-plugin-pwa" // Helper imports
 
-import { manifest, seoConfig } from "./utils/seoConfig";
-import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless"; // https://astro.build/config
+import { manifest, seoConfig } from "./utils/seoConfig"
+import react from "@astrojs/react"
+import vercel from "@astrojs/vercel/serverless" // https://astro.build/config
 
-import node from "@astrojs/node";
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
-  site: seoConfig.baseURL,
-  output: "server",
-  server: {
-    port: 3005
-  },
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false,
-      path: "./tailwind.config.js"
-    }
-  }), sitemap(), compress(), react()],
-  vite: {
-    plugins: [VitePWA({
-      registerType: "autoUpdate",
-      manifest,
-      workbox: {
-        globDirectory: "dist",
-        globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
-        // Don't fallback on document based (e.g. `/some-page`) requests
-        // This removes an errant console.log message from showing up.
-        navigateFallback: null
-      }
-    })]
-  },
-  adapter: node({
-    mode: "standalone"
-  })
-});
+	site: seoConfig.baseURL,
+	output: "server",
+	server: {
+		port: 3005
+	},
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+				path: "./tailwind.config.js"
+			}
+		}),
+		sitemap(),
+		compress(),
+		react()
+	],
+	vite: {
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+					globDirectory: "dist",
+					globPatterns: [
+						"**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"
+					],
+					// Don't fallback on document based (e.g. `/some-page`) requests
+					// This removes an errant console.log message from showing up.
+					navigateFallback: null
+				}
+			})
+		]
+	},
+	adapter: vercel()
+})
