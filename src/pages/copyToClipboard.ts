9949c6bc43a0a5f831ef5afdabf46d891f935471
nkumnick <nkumnick@oev.de>
2023-04-11 10:12:22 +0200

    suppresss error in copytoclipboard

--- src/pages/copyToClipboard.ts
+++ src/pages/copyToClipboard.ts
@@ -2,6 +2,7 @@
 const buttons = document.querySelectorAll("[data-copy]")
 buttons.forEach((button) =>
 	button.addEventListener("click", (ev) => {
+		// @ts-ignore
 		navigator.clipboard.writeText(ev.target.value)
 	})
 )
