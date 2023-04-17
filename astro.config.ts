f2f716f7f56ebf8d485e401a066b66e63c1bb43f
nkumnick <nkumnick@oev.de>
2023-04-17 22:05:14 +0200

    static may be the solution?

--- astro.config.ts
+++ astro.config.ts
@@ -7,8 +7,8 @@ import { VitePWA } from "vite-plugin-pwa" // Helper imports
 
 import { manifest, seoConfig } from "./utils/seoConfig"
 import react from "@astrojs/react"
-import vercel from "@astrojs/vercel/serverless" // https://astro.build/config
-
+// import vercel from "@astrojs/vercel/serverless" // https://astro.build/config
+import vercelStatic from "@astrojs/vercel/static"
 import node from "@astrojs/node"
 
 // https://astro.build/config
@@ -49,5 +49,5 @@ export default defineConfig({
 			})
 		]
 	},
-	adapter: vercel()
+	adapter: vercelStatic()
 })
